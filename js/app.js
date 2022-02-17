424eb4954ee4ec41b251a4268608f6eeba1008d5
mahabub ahmed <mahabub.ahmed.eee@gmail.com>
2022-02-17 12:20:14 +0100

    third commit

--- js/app.js
+++ js/app.js
@@ -1,39 +1,76 @@
 function getInputValue(inputId){
-    const input = document.getElementById(inputId).value;
-    const amount = parseFloat(input); 
+    const input = document.getElementById(inputId);
+    const inputText = input.value;
+    const amount = parseFloat(inputText); 
     return amount;
 }
-// const incomeAmountInput = document.getElementById('income-input').value;
-// const incomeAmount = parseFloat(incomeAmountInput); 
-//handle calculation button
-document.getElementById('calculate-btn').addEventListener('click', function (){
-    const incomeAmount = getInputValue('income-input'); 
+
+//subtract function
+
+function calcExpenses(){
+    
     const foodExpense = getInputValue('food-expense'); 
     const rentExpense = getInputValue('rent-expense'); 
-    const clothesExpense = getInputValue('clothes-expense'); 
-    const expensesAmount = document.getElementById('total-expenses');
-    const totalExpenses = foodExpense + rentExpense + clothesExpense;
-    expensesAmount.innerText = totalExpenses;
-   
-    const balance = document.getElementById('balance');
-    balance.innerText = incomeAmount - totalExpenses;
+    const clothesExpense = getInputValue('clothes-expense');
+    if(foodExpense > 0 && foodExpense != isNaN && rentExpense > 0 && rentExpense != isNaN && clothesExpense > 0 && clothesExpense != isNaN){
+        const totalExpenses = foodExpense + rentExpense + clothesExpense;
+        return totalExpenses;
+    } 
+    else{
+        alert('Please eneter a valid number');
+    }
+    
+}
 
+function remaining(){
+    const incomeAmount = getInputValue('income-input');  
+    const remainingValue = incomeAmount - calcExpenses();
+    return remainingValue;
+}
+
+document.getElementById('calculate-btn').addEventListener('click', function (){
+    
+    const expensesAmount = document.getElementById('total-expenses');
+    const totalExpenses = calcExpenses()
+    expensesAmount.innerText = totalExpenses;
+    //balance calculation
+    const incomeAmount = getInputValue('income-input');  
+    const balance = document.getElementById('balance');
+    if(incomeAmount > 0 && incomeAmount != isNaN && incomeAmount > totalExpenses){
+        const remainingValue = remaining(incomeAmount, totalExpenses);
+        balance.innerText = remainingValue
+    }
+    else{
+        if(incomeAmount < 0 && incomeAmount != isNaN){
+            alert('Please enter a valid number')
+        }
+        else{
+            alert('Your expenses is higher than income')
+        }
+    }
 })
 //handle save button
 document.getElementById('save-btn').addEventListener('click', function (){
-    const incomeAmount = getInputValue();
-    const parcentageInput = document.getElementById('save-input');
-    const parcentageInputText = parcentageInput.value;
-    const parcentageValue = parseFloat(parcentageInputText);
-    const parcentageAmount = parcentageValue / 100;
-    const savingAmountValue = incomeAmount * parcentageAmount;
-    const savingAmount = document.getElementById('saving-amount');
-    savingAmount.innerText = savingAmountValue;
-    console.log(savingAmountValue)
-    //remaining balance
-    const remainingBalance = document.getElementById('remaining-balance');
-    remainingBalance.innerText = balance.innerText - savingAmount;
-
-
+    const incomeAmount = getInputValue('income-input');
+    const parcentageInput = getInputValue('save-input')
+    if(parcentageInput > 0 && parcentageInput != isNaN){
+        const savingAmountValue = (parcentageInput / 100) * incomeAmount; 
+        const savingAmount = document.getElementById('saving-amount');
+        savingAmount.innerText = savingAmountValue;
+        //remaining balance
+        const remainingBalance = document.getElementById('ramaining-balance');
+        const remainingValue = remaining(incomeAmount, calcExpenses());
+        if(remainingValue >= savingAmountValue){
+        remainingBalance.innerText = remainingValue - savingAmountValue;
+        }
+        else{
+        alert('your savings amount can not exceed your balance');
+        }
+        }
+    else{
+        alert('Please enter a valid number')
+    }
+    
+    
 })
 
